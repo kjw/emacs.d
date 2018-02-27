8d25f3c092357bf91b72effa9de035a29be56cb9
Karl Jonathan Ward <karl.j.ward@gmail.com>
2018-02-27 13:45:37 +0000

    Drop symlinks

--- init.el
+++ init.el
@@ -1,1 +1,12 @@
-/Users/karl/Dropbox/Config/emacs/init.el
+;; Added by package.el, just leave here
+
+(package-initialize)
+
+;; Load literate config
+
+(require 'org)
+(require 'ob-tangle)
+
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "config.org" init-dir))
+
